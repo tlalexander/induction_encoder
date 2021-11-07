5bca1c27713c45ec7118bf46af0341dd5712a51b
Taylor <tlalexander@gmail.com>
2021-11-06 23:57:28 -0700

    Complete loops that double back on one side using vias. Nets may be incorrect.

--- spiral_pcb.py
+++ spiral_pcb.py
@@ -13,7 +13,7 @@ outside_radius = 47
 width = outside_radius-inside_radius
 
 phases = 8
-loops = 20
+# loops = 20
 loops = 10
 steps = 30
 loop_angle = 2*math.pi/loops
@@ -35,18 +35,25 @@ phase_values = [[],[],[],[]]
 
 tx, rx1, rx2 = Net('TX'), Net('RX1'), Net('RX2')
 
-nets=[tx, rx1, rx2,tx, rx1, rx2, rx1, rx2]
+nets=[rx1, rx1, rx2, rx2, rx1, rx1, rx2, rx2]
 
 segments = []
 
 phase_layers = ['F.Cu', 'Inner1.Cu', 'B.Cu']
 
 via_list = []
-last_point = [None, None, None,None, None, None, None, None]
+last_point = [None, None, None, None, None, None, None, None]
 
 for loopnum in range(loops):
     for phasenum in range(phases):
-        for idx in range(steps):
+        for idx in range(steps+1):
+
+            if idx == steps and loopnum!=loops-1:
+                # The extra step on the last loop closes a gap created
+                # because last_point was none for the very first point.
+                continue
+
+
             factor = (math.sin(idx/steps*math.pi*2) + 1)/2
             angle = loop_angle*(idx/steps)+loopnum*loop_angle+phasenum*phase_angle + angle_offset
             radial_point_distance = inside_radius + factor * width
@@ -54,21 +61,35 @@ for loopnum in range(loops):
             x_value = math.cos(angle) * radial_point_distance + center_offset_y
             current_point = [x_value, y_value]
 
+            top_layer = True
+
             if last_point[phasenum]:
                 if idx<=steps/4:
-                    layer = 'F.Cu'
+                    top_layer = True
                 if idx==int(steps/4) or idx==int(3*steps/4):
                     via_list.append(Via(at=current_point, size=.8, drill=.6, net=nets[phasenum].code))
                 if idx>steps/4 and idx < 3*steps/4:
-                    layer = 'B.Cu'
+                    top_layer = False
                 if idx>3*steps/4:
-                    layer = 'F.Cu'
+                    top_layer = True
+
+                if loopnum == int(loops/2)-1 and phasenum>=phases/2:
+                    if idx==int(steps/2)+1:
+                        via_list.append(Via(at=current_point, size=.8, drill=.6, net=nets[phasenum].code))
+                        top_layer = not top_layer
+
+                if loopnum == int(loops/2) and phasenum<phases/2:
+                    if idx==0:
+                        via_list.append(Via(at=last_point[phasenum], size=.8, drill=.6, net=nets[phasenum].code))
+                        top_layer = not top_layer
+
+                layer = 'B.Cu'if top_layer else 'F.Cu'
                 segments.append(Segment(start=last_point[phasenum], end=current_point, layer=layer, net=nets[phasenum].code))
 
             last_point[phasenum] = current_point
 
             # phase_values[phasenum].append([x_value, y_value])
-
+# print(last_point)
 # for phasenum in range(phases):
 #     for idx in range(len(phase_values[phasenum])-1):
 #         start = phase_values[phasenum][idx]

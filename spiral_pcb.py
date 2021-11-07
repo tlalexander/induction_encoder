1a800b72eef087fa484abdf76b1555fac2b401c8
Taylor <tlalexander@gmail.com>
2021-11-06 20:45:44 -0700

    Spiral pattern matches reference design now.

--- spiral_pcb.py
+++ spiral_pcb.py
@@ -9,14 +9,16 @@ center_offset_x = 100
 center_offset_y = 100
 
 inside_radius = 33
-outside_radius = 48
+outside_radius = 47
 width = outside_radius-inside_radius
 
-phases = 3
+phases = 8
 loops = 20
+loops = 10
 steps = 30
 loop_angle = 2*math.pi/loops
 phase_angle = loop_angle/phases
+angle_offset = 0-phase_angle/2.5
 
 x_values = []
 y_values = []
@@ -31,38 +33,55 @@ point_colors = []
 
 phase_values = [[],[],[],[]]
 
-for loopnum in range(loops):
-    for phasenum in range(phases):
-        for idx in range(steps):
-            factor = (math.sin(idx/steps*math.pi*2) + 1)/2
-            angle = loop_angle*(idx/steps)+loopnum*loop_angle+phasenum*phase_angle
-            radial_point_distance = inside_radius + factor * width
-            y_value = math.sin(angle) * radial_point_distance + center_offset_x
-            x_value = math.cos(angle) * radial_point_distance + center_offset_y
-
-            phase_values[phasenum].append([x_value, y_value])
-
-
 tx, rx1, rx2 = Net('TX'), Net('RX1'), Net('RX2')
 
-nets=[tx, rx1, rx2]
+nets=[tx, rx1, rx2,tx, rx1, rx2, rx1, rx2]
 
 segments = []
 
 phase_layers = ['F.Cu', 'Inner1.Cu', 'B.Cu']
 
-for phasenum in range(phases):
-    for idx in range(len(phase_values[phasenum])-1):
-        start = phase_values[phasenum][idx]
-        end = phase_values[phasenum][idx+1]
-        seg = Segment(start=start, end=end, layer=phase_layers[phasenum], net=nets[phasenum].code)
-        segments.append(seg)
+via_list = []
+last_point = [None, None, None,None, None, None, None, None]
 
+for loopnum in range(loops):
+    for phasenum in range(phases):
+        for idx in range(steps):
+            factor = (math.sin(idx/steps*math.pi*2) + 1)/2
+            angle = loop_angle*(idx/steps)+loopnum*loop_angle+phasenum*phase_angle + angle_offset
+            radial_point_distance = inside_radius + factor * width
+            y_value = math.sin(angle) * radial_point_distance + center_offset_x
+            x_value = math.cos(angle) * radial_point_distance + center_offset_y
+            current_point = [x_value, y_value]
+
+            if last_point[phasenum]:
+                if idx<=steps/4:
+                    layer = 'F.Cu'
+                if idx==int(steps/4) or idx==int(3*steps/4):
+                    via_list.append(Via(at=current_point, size=.8, drill=.6, net=nets[phasenum].code))
+                if idx>steps/4 and idx < 3*steps/4:
+                    layer = 'B.Cu'
+                if idx>3*steps/4:
+                    layer = 'F.Cu'
+                segments.append(Segment(start=last_point[phasenum], end=current_point, layer=layer, net=nets[phasenum].code))
+
+            last_point[phasenum] = current_point
+
+            # phase_values[phasenum].append([x_value, y_value])
+
+# for phasenum in range(phases):
+#     for idx in range(len(phase_values[phasenum])-1):
+#         start = phase_values[phasenum][idx]
+#         end = phase_values[phasenum][idx+1]
+#         seg = Segment(start=start, end=end, layer=phase_layers[phasenum], net=nets[phasenum].code)
+#         segments.append(seg)
+coords = [(0, 0), (10, 0), (10, 10), (0, 10)]
+gndplane_top = Zone(net_name='GND', layer='F.Cu', polygon=coords, clearance=0.3)
 
 layers = [
 Layer('F.Cu'),
-Layer('Inner1.Cu'),
-Layer('Inner2.Cu'),
+# Layer('Inner1.Cu'),
+# Layer('Inner2.Cu'),
 Layer('B.Cu'),
 Layer('Edge.Cuts', type='user')
 ]
@@ -72,20 +91,22 @@ for layer in ['Mask', 'Paste', 'SilkS', 'CrtYd', 'Fab']:
         layers.append(Layer('%s.%s' % (side, layer), type='user'))
         nc1 = NetClass('default', trace_width=1, nets=['TX', 'RX1', 'RX2'])
 
+# print(via_list)
+
 # Create PCB
 pcb = Pcb()
 pcb.title = 'A title'
 pcb.comment1 = 'Comment 1'
 pcb.page_type = [200, 200]
-pcb.num_nets = 5
+pcb.num_nets = 3
 pcb.setup = Setup(grid_origin=[10, 10])
 pcb.layers = layers
 # pcb.modules += [r1, r2]
 pcb.net_classes += [nc1]
 pcb.nets += nets
 pcb.segments += segments
-# pcb.vias += [v1]
-# pcb.zones += [gndplane_top]
+pcb.vias += via_list
+pcb.zones += [gndplane_top]
 
 pcb.to_file('project')
 
